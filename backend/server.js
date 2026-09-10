da1a088d518c0e5b2659373a7a23d9a8cd4f265c
jakkaswapnikakrishna <jakkaswapnikakrishna@gmail.com>
2026-09-10 11:16:52 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -777,6 +777,8 @@ app.get("/api/dataset/analyze/:sceneId", async (req, res) => {
     });
   }
 });
-app.listen(5000, () => {
-  console.log("🚀 SatQuery backend running on http://localhost:5000");
-}); 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
